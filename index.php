5f1585fae68c7d143ddef5e47b24ad630e69566f
rangaimal <rangaimal1234@gmail.com>
2018-04-12 09:50:44 +0530

    log in form

--- index.php
+++ index.php
@@ -2,14 +2,42 @@
  <?php
  //check the form submission
 if(isset($_POST['submit'])){
+
+	$errors=array();
 //check the user name and password enter
 	if(!isset($_POST['email']) || strlen(trim($_POST['email'])) <1){
-		
+		$errors[]='User name is missing/invalid';
 	}
 
+if(!isset($_POST['password']) || strlen(trim($_POST['password'])) <1){
+		$errors[]='password is missing/invalid';
+	}
+//check there are any errors in the form
+	if (isset($errors)) {
+//save user name and password in to the variabeles
+      $email=mysqli_real_escape_string($connection,$_POST['email']);
+      $password=mysqli_real_escape_string($connection,$_POST['password']);
+      $hashed_password= sha1($password);
+		
+	//prepare database query
+	$query="SELECT * FROM user WHERE email='{$email}' AND hashed_password='{$hashed_password}' LIMIT 1";
 
+	$result_set=mysqli_query($connection,$query);
 
+	if($result_set){
+		//query succesful;
 
+		if(mysqli_num_rows($result_set)==1){
+			//valid user found
+			//redirect to user.php
+			header('Location:users.php');
+		}else 
+		//user name and password invalid
+		$errors[]='invalid username/password ';
+	} else {
+		$errors[]='Database query faild';
+	}	
+	}
 }
 
  ?>
@@ -22,12 +50,16 @@ if(isset($_POST['submit'])){
 <body>
 <div class="login">
 
-	<form action="index.php" method="post"> </form>
+	<form action="index.php" method="post"> 
 
 	<fieldset>
 
 		<legend><h1> Log In</h1></legend>
-
+		<?php 
+			if(isset($errors) && !empty($errors)){
+				echo '<p class="error"> Invalid username/password </p>';
+			}
+		?>
 <p>
 	<label for="">Username:</label>
 	<input type="text" name="email" id="" placeholder="email address">
@@ -40,7 +72,7 @@ if(isset($_POST['submit'])){
 	<button type="submit" name="submit">Log in</button>
 </p>
 	</fieldset>
-	
+	</form>
 </div>
 </body>
 </html>
